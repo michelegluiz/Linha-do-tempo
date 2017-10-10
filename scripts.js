90933eaf63223d29934ecaa6f7be9761f80d12b5
Michele <michele.gl35@gmail.com>
2017-10-10 19:21:06 -0300

    site linha do tempo

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let modal = document.querySelector('.modal');
+
+
+function alternarModal() {
+  if (modal.style.display == 'block') {
+    modal.style.display = 'none';} 
+  else {modal.style.display = 'block';}
+}
